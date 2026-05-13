d2a27e6946318d473d11059089f6beb67760a09f
engmater6-dev <engmater6@gmail.com>
2026-05-14 07:53:25 +0900

    test 완료

--- cpp/test/GildedRoseTest.cpp
+++ cpp/test/GildedRoseTest.cpp
@@ -1,56 +1,61 @@
 #include "GildedRose.h"
 #include <gtest/gtest.h>
 
+// TC01
 TEST(GildedRoseTest, Foo) {
   std::vector<Item> items = {Item("foo", 0, 0)};
   GildedRose app(items);
   app.updateQuality();
   EXPECT_EQ("foo", app.items[0].name);
-  EXPECT_EQ(-1, app.items[0].sellIn);
-  EXPECT_EQ(0, app.items[0].quality);
 }
 
+// TC02 : 유통기한 내 1씩 감소
 TEST(GildedRoseTest, Foo2) {
   std::vector<Item> items = {Item("foo", 2, 5)};
   GildedRose app(items);
   app.updateQuality();
-  EXPECT_EQ("foo", app.items[0].name);
   EXPECT_EQ(1, app.items[0].sellIn);
   EXPECT_EQ(4, app.items[0].quality);
 }
 
+// TC03 : 유통기한 지난제품 2씩 감소
 TEST(GildedRoseTest, Foo3) {
   std::vector<Item> items = {Item("foo", 0, 5)};
   GildedRose app(items);
   app.updateQuality();
-  EXPECT_EQ("foo", app.items[0].name);
   EXPECT_EQ(-1, app.items[0].sellIn);
   EXPECT_EQ(3, app.items[0].quality);
 }
 
+// TC04 : Aged Brie 유통기한 내 1씩 증가
 TEST(GildedRoseTest, AgedBrie) {
   std::vector<Item> items = {Item("Aged Brie", 2, 5)};
   GildedRose app(items);
   app.updateQuality();
-  EXPECT_EQ("Aged Brie", app.items[0].name);
-  EXPECT_EQ(1, app.items[0].sellIn);
   EXPECT_EQ(6, app.items[0].quality);
 }
 
+// TC05 : Aged Brie 유통기한 지난제품 2씩 증가
 TEST(GildedRoseTest, AgedBrie2) {
   std::vector<Item> items = {Item("Aged Brie", 0, 5)};
   GildedRose app(items);
   app.updateQuality();
-  EXPECT_EQ("Aged Brie", app.items[0].name);
-  EXPECT_EQ(-1, app.items[0].sellIn);
   EXPECT_EQ(7, app.items[0].quality);
 }
 
+// TC06 : Aged Brie 품질은 50이 최대
 TEST(GildedRoseTest, AgedBrie3) {
   std::vector<Item> items = {Item("Aged Brie", 2, 50)};
   GildedRose app(items);
   app.updateQuality();
-  EXPECT_EQ("Aged Brie", app.items[0].name);
-  EXPECT_EQ(1, app.items[0].sellIn);
   EXPECT_EQ(50, app.items[0].quality);
+}
+
+// TC07 : Sulfuras는 품질과 유통기한이 변하지 않음
+TEST(GildedRoseTest, Sulfuras) {
+  std::vector<Item> items = {Item("Sulfuras, Hand of Ragnaros", 1, 5)};
+  GildedRose app(items);
+  app.updateQuality();
+  EXPECT_EQ(1, app.items[0].sellIn);
+  EXPECT_EQ(5, app.items[0].quality);
 }
