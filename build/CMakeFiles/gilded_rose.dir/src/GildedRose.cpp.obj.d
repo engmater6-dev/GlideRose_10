6943c396327a90e854cec3b70911dadb868c83e5
engmater6-dev <engmater6@gmail.com>
2026-05-15 11:40:46 +0900

    c++ 수정 후 test 1차 fail..

--- build/CMakeFiles/gilded_rose.dir/src/GildedRose.cpp.obj.d
+++ build/CMakeFiles/gilded_rose.dir/src/GildedRose.cpp.obj.d
@@ -105,4 +105,12 @@ CMakeFiles/gilded_rose.dir/src/GildedRose.cpp.obj: \
  C:/mingw64/include/c++/15.2.0/bits/stl_uninitialized.h \
  C:/mingw64/include/c++/15.2.0/bits/stl_vector.h \
  C:/mingw64/include/c++/15.2.0/bits/stl_bvector.h \
- C:/mingw64/include/c++/15.2.0/bits/vector.tcc
+ C:/mingw64/include/c++/15.2.0/bits/vector.tcc \
+ C:/mingw64/include/c++/15.2.0/algorithm \
+ C:/mingw64/include/c++/15.2.0/bits/stl_algo.h \
+ C:/mingw64/include/c++/15.2.0/bits/algorithmfwd.h \
+ C:/mingw64/include/c++/15.2.0/bits/stl_heap.h \
+ C:/mingw64/include/c++/15.2.0/bits/uniform_int_dist.h \
+ C:/mingw64/include/c++/15.2.0/bits/stl_tempbuf.h \
+ C:/mingw64/include/c++/15.2.0/pstl/glue_algorithm_defs.h \
+ C:/mingw64/include/c++/15.2.0/pstl/execution_defs.h
--- cpp/src/GildedRose.cpp
+++ cpp/src/GildedRose.cpp
@@ -1,56 +1,95 @@
 #include "GildedRose.h"
+#include <algorithm>
 
-GildedRose::GildedRose(std::vector<Item>& items) : items(items) {}
+// 전역 상수
+constexpr int QUALITY_MIN = 0;
+constexpr int QUALITY_MAX = 50;
+
+// 전역 함수
+inline void increase_quality(Item &item, int amount = 1) {
+  item.quality = std::min(QUALITY_MAX, item.quality + amount);
+}
+
+inline void decrease_quality(Item &item, int amount = 1) {
+  item.quality = std::max(QUALITY_MIN, item.quality - amount);
+}
+
+inline void decrease_sellin(Item &item) { item.sellIn -= 1; }
+
+// 추상 클래스
+class ItemUpdate {
+protected:
+  Item &item;
+
+public:
+  ItemUpdate(Item &i) : item(i) {}
+  virtual ~ItemUpdate() {}
+  virtual void update_quality() = 0;
+  virtual void decrease_sellin() { ::decrease_sellin(item); }
+};
+
+// AgedBrie
+class AgedBrie : public ItemUpdate {
+public:
+  AgedBrie(Item &i) : ItemUpdate(i) {}
+  void update_quality() override { increase_quality(item, 1); }
+};
+
+// BackStage
+class BackStage : public ItemUpdate {
+public:
+  BackStage(Item &i) : ItemUpdate(i) {}
+  void update_quality() override {
+    int amount = (item.sellIn >= 11) ? 1 : (item.sellIn > 5 ? 2 : 3);
+    increase_quality(item, amount);
+    if (item.sellIn < 0)
+      item.quality = QUALITY_MIN;
+  }
+};
+
+// Sulfuras
+class Sulfuras : public ItemUpdate {
+public:
+  Sulfuras(Item &i) : ItemUpdate(i) {}
+  void update_quality() override { /* 변하지 않음 */ }
+  void decrease_sellin() override { /* 변하지 않음 */ }
+};
+
+// Conjured
+class Conjured : public ItemUpdate {
+public:
+  Conjured(Item &i) : ItemUpdate(i) {}
+  void update_quality() override { decrease_quality(item, 2); }
+};
+
+// Food & Beverage (기본 아이템)
+class FoodBeverage : public ItemUpdate {
+public:
+  FoodBeverage(Item &i) : ItemUpdate(i) {}
+  void update_quality() override { decrease_quality(item, 1); }
+};
+
+// GildedRose 구현
+GildedRose::GildedRose(std::vector<Item> &items) : items(items) {}
 
 void GildedRose::updateQuality() {
-    for (size_t i = 0; i < items.size(); i++) {
-        if (items[i].name != "Aged Brie"
-                && items[i].name != "Backstage passes to a TAFKAL80ETC concert") {
-            if (items[i].quality > 0) {
-                if (items[i].name != "Sulfuras, Hand of Ragnaros") {
-                    items[i].quality = items[i].quality - 1;
-                }
-            }
-        } else {
-            if (items[i].quality < 50) {
-                items[i].quality = items[i].quality + 1;
+  for (auto &item : items) {
+    ItemUpdate *updater = nullptr;
 
-                if (items[i].name == "Backstage passes to a TAFKAL80ETC concert") {
-                    if (items[i].sellIn < 11) {
-                        if (items[i].quality < 50) {
-                            items[i].quality = items[i].quality + 1;
-                        }
-                    }
-
-                    if (items[i].sellIn < 6) {
-                        if (items[i].quality < 50) {
-                            items[i].quality = items[i].quality + 1;
-                        }
-                    }
-                }
-            }
-        }
-
-        if (items[i].name != "Sulfuras, Hand of Ragnaros") {
-            items[i].sellIn = items[i].sellIn - 1;
-        }
-
-        if (items[i].sellIn < 0) {
-            if (items[i].name != "Aged Brie") {
-                if (items[i].name != "Backstage passes to a TAFKAL80ETC concert") {
-                    if (items[i].quality > 0) {
-                        if (items[i].name != "Sulfuras, Hand of Ragnaros") {
-                            items[i].quality = items[i].quality - 1;
-                        }
-                    }
-                } else {
-                    items[i].quality = items[i].quality - items[i].quality;
-                }
-            } else {
-                if (items[i].quality < 50) {
-                    items[i].quality = items[i].quality + 1;
-                }
-            }
-        }
+    if (item.name == "Aged Brie") {
+      updater = new AgedBrie(item);
+    } else if (item.name == "Backstage passes to a TAFKAL80ETC concert") {
+      updater = new BackStage(item);
+    } else if (item.name == "Sulfuras, Hand of Ragnaros") {
+      updater = new Sulfuras(item);
+    } else if (item.name == "Conjured") {
+      updater = new Conjured(item);
+    } else {
+      updater = new FoodBeverage(item);
     }
+
+    updater->update_quality();
+    updater->decrease_sellin();
+    delete updater;
+  }
 }
